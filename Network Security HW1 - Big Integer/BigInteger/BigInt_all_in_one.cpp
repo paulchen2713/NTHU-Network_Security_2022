99b939f03ed296f278c384d094af6fdbe13f2fe7
Paul <95068443+paulchen2713@users.noreply.github.com>
2023-02-03 11:20:07 +0800

    338

--- Network Security HW1 - Big Integer/BigInteger/BigInt_all_in_one.cpp
+++ Network Security HW1 - Big Integer/BigInteger/BigInt_all_in_one.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class BigInt{
+class BigInt {
 	std::string digits;
 public:
 	//Constructors:
@@ -102,8 +102,9 @@ BigInt::BigInt(BigInt & a) {
 }
 
 bool Null(const BigInt& a) {
-	if(a.digits.size() == 1 && a.digits[0] == 0)
+	if (a.digits.size() == 1 && a.digits[0] == 0) {
 		return true;
+	}
 	return false;
 }
 int Length(const BigInt & a) {
@@ -144,24 +145,24 @@ BigInt &BigInt::operator=(const BigInt &a) {
 	return *this;
 }
 
-BigInt &BigInt::operator++(){
+BigInt &BigInt::operator++() {
 	int i, n = digits.size();
 	for (i = 0; i < n && digits[i] == 9;i++)
 		digits[i] = 0;
-	if(i == n)
+	if (i == n) {
 		digits.push_back(1);
-	else
-		digits[i]++;
+	}
+	else digits[i]++;
 	return *this;
 }
-BigInt BigInt::operator++(int temp){
+BigInt BigInt::operator++(int temp) {
 	BigInt aux;
 	aux = *this;
 	++(*this);
 	return aux;
 }
 
-BigInt &BigInt::operator--(){
+BigInt &BigInt::operator--() {
 	if(digits[0] == 0 && digits.size() == 1)
 		throw("UNDERFLOW");
 	int i, n = digits.size();
@@ -172,29 +173,29 @@ BigInt &BigInt::operator--(){
 		digits.pop_back();
 	return *this;
 }
-BigInt BigInt::operator--(int temp){
+BigInt BigInt::operator--(int temp) {
 	BigInt aux;
 	aux = *this;
 	--(*this);
 	return aux;
 }
 
-BigInt &operator+=(BigInt &a,const BigInt& b){
+BigInt &operator+=(BigInt &a,const BigInt& b) {
 	int t = 0, s, i;
 	int n = Length(a), m = Length(b);
-	if(m > n)
+	if (m > n) {
 		a.digits.append(m - n, 0);
+	}
 	n = Length(a);
-	for (i = 0; i < n;i++){
-		if(i < m)
+	for (i = 0; i < n; i++) {
+		if (i < m) {
 			s = (a.digits[i] + b.digits[i]) + t;
-		else
-			s = a.digits[i] + t;
+		}
+		else s = a.digits[i] + t;
 		t = s / 10;
 		a.digits[i] = (s % 10);
 	}
-	if(t)
-		a.digits.push_back(t);
+	if (t) a.digits.push_back(t);
 	return a;
 }
 BigInt operator+(const BigInt &a, const BigInt &b){
@@ -226,53 +227,54 @@ BigInt &operator-=(BigInt&a,const BigInt &b){
 		n--;
 	return a;
 }
-BigInt operator-(const BigInt& a,const BigInt&b){
+BigInt operator-(const BigInt& a, const BigInt&b) {
 	BigInt temp;
 	temp = a;
 	temp -= b;
 	return temp;
 }
 
-BigInt &operator*=(BigInt &a, const BigInt &b)
-{
-	if(Null(a) || Null(b)){
+BigInt &operator*=(BigInt &a, const BigInt &b) {
+	if (Null(a) || Null(b)) {
 		a = BigInt();
 		return a;
 	}
 	int n = a.digits.size(), m = b.digits.size();
 	vector<int> v(n + m, 0);
-	for (int i = 0; i < n;i++)
-		for (int j = 0; j < m;j++){
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
 			v[i + j] += (a.digits[i] ) * (b.digits[j]);
 		}
+	}
 	n += m;
 	a.digits.resize(v.size());
-	for (int s, i = 0, t = 0; i < n; i++)
-	{
+	for (int s, i = 0, t = 0; i < n; i++) {
 		s = t + v[i];
 		v[i] = s % 10;
 		t = s / 10;
 		a.digits[i] = v[i] ;
 	}
-	for (int i = n - 1; i >= 1 && !v[i];i--)
-			a.digits.pop_back();
+	for (int i = n - 1; i >= 1 && !v[i]; i--) {
+		a.digits.pop_back();
+	}
 	return a;
 }
-BigInt operator*(const BigInt&a,const BigInt&b){
+BigInt operator*(const BigInt& a, const BigInt& b) {
 	BigInt temp;
 	temp = a;
 	temp *= b;
 	return temp;
 }
 
-BigInt &operator/=(BigInt& a,const BigInt &b){
-	if(Null(b))
+BigInt &operator/=(BigInt& a,const BigInt &b) {
+	if (Null(b)) {
 		throw("Arithmetic Error: Division By 0");
-	if(a < b){
+	}
+	if (a < b) {
 		a = BigInt();
 		return a;
 	}
-	if(a == b){
+	if (a == b) {
 		a = BigInt(1);
 		return a;
 	}
@@ -280,37 +282,39 @@ BigInt &operator/=(BigInt& a,const BigInt &b){
 	int n = Length(a), m = Length(b);
 	vector<int> cat(n, 0);
 	BigInt t;
-	for (i = n - 1; t * 10 + a.digits[i] < b;i--){
+	for (i = n - 1; t * 10 + a.digits[i] < b; i--) {
 		t *= 10;
 		t += a.digits[i] ;
 	}
-	for (; i >= 0; i--){
+	for (; i >= 0; i--) {
 		t = t * 10 + a.digits[i];
 		for (cc = 9; cc * b > t;cc--);
 		t -= cc * b;
 		cat[lgcat++] = cc;
 	}
 	a.digits.resize(cat.size());
-	for (i = 0; i < lgcat;i++)
+	for (i = 0; i < lgcat;i++) {
 		a.digits[i] = cat[lgcat - i - 1];
+	}
 	a.digits.resize(lgcat);
 	return a;
 }
-BigInt operator/(const BigInt &a,const BigInt &b){
+BigInt operator/(const BigInt &a, const BigInt &b) {
 	BigInt temp;
 	temp = a;
 	temp /= b;
 	return temp;
 }
 
-BigInt &operator%=(BigInt& a,const BigInt &b){
-	if(Null(b))
+BigInt &operator%=(BigInt& a,const BigInt &b) {
+	if (Null(b)) {
 		throw("Arithmetic Error: Division By 0");
-	if(a < b){
+	}
+	if (a < b) {
 		a = BigInt();
 		return a;
 	}
-	if(a == b){
+	if (a == b) {
 		a = BigInt(1);
 		return a;
 	}
@@ -318,20 +322,20 @@ BigInt &operator%=(BigInt& a,const BigInt &b){
 	int n = Length(a), m = Length(b);
 	vector<int> cat(n, 0);
 	BigInt t;
-	for (i = n - 1; t * 10 + a.digits[i] < b;i--){
+	for (i = n - 1; t * 10 + a.digits[i] < b; i--) {
 		t *= 10;
 		t += a.digits[i];
 	}
-	for (; i >= 0; i--){
+	for (; i >= 0; i--) {
 		t = t * 10 + a.digits[i];
-		for (cc = 9; cc * b > t;cc--);
+		for (cc = 9; cc * b > t; cc--);
 		t -= cc * b;
 		cat[lgcat++] = cc;
 	}
 	a = t;
 	return a;
 }
-BigInt operator%(const BigInt &a,BigInt &b){
+BigInt operator%(const BigInt &a, BigInt &b) {
 	BigInt temp;
 	temp = a;
 	temp %= b;
